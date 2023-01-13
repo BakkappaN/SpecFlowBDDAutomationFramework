5d44d4b76d50b19085bb7f1d49d259d05caf45d0
BakkappaN <bakkappac@gmail.com>
2023-01-13 23:40:03 +0530

    specflow bdd automation framework initial commit

---
+++ Features/Feature1.feature
@@ -0,0 +1,11 @@
+﻿Feature: Verify youtube search feature
+
+Search in the youtube
+
+@TestersTalk
+Scenario: Search for Testers Talk
+	Given Open the browser
+	When Enter the URL
+	Then Search for the Testers Talk
+
+
---
+++ StepDefinitions/Feature1StepDefinitions.cs
@@ -0,0 +1,36 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SpecFlowBDDAutomationFramework.StepDefinitions
+{
+    [Binding]
+    public sealed class Feature1StepDefinitions
+    {
+        private IWebDriver driver;
+
+        [Given(@"Open the browser")]
+        public void GivenOpenTheBrowser()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [When(@"Enter the URL")]
+        public void WhenEnterTheURL()
+        {
+            driver.Url = "https://www.youtube.com/";
+            Thread.Sleep(5000);
+            
+        }
+
+        [Then(@"Search for the Testers Talk")]
+        public void ThenSearchForTheTestersTalk()
+        {
+            driver.FindElement(By.XPath("//*[@name='search_query']")).SendKeys(" Testers Talk ");
+            driver.FindElement(By.XPath("//*[@name='search_query']")).SendKeys(Keys.Enter);
+            Thread.Sleep(5000);
+            driver.Quit();
+        }
+
+    }
+}
